f41d5f8d825e4c7550c06eec3e6e97cc696e91f9
solexrd106 <1325716331@qq.com>
2018-11-15 16:54:47 +0800

    statusbars

--- app/src/main/java/qin/com/coodinatorqin/collapsetest/CollapseTestActivity.java
+++ app/src/main/java/qin/com/coodinatorqin/collapsetest/CollapseTestActivity.java
@@ -1,17 +1,24 @@
 package qin.com.coodinatorqin.collapsetest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import qin.com.coodinatorqin.R;
 
 public class CollapseTestActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collapse_test);
+        ButterKnife.bind(this);
 
-
+        setSupportActionBar(toolbar);
     }
 }
--- app/src/main/java/qin/com/coodinatorqin/main/adapter/MainAdapter.java
+++ app/src/main/java/qin/com/coodinatorqin/main/adapter/MainAdapter.java
@@ -18,6 +18,7 @@ import qin.com.coodinatorqin.appbar.AppBarLayoutActivity;
 import qin.com.coodinatorqin.bottomsheetbar.BottomSheetBaiduActivity;
 import qin.com.coodinatorqin.collapse.collapseLayoutActivity;
 import qin.com.coodinatorqin.collapsesnap.CollapseSnapActivity;
+import qin.com.coodinatorqin.collapsetest.CollapseTestActivity;
 import qin.com.coodinatorqin.floatbutton.FloatButtonActivity;
 import qin.com.coodinatorqin.main.Viewholder.DataBindingViewHolder;
 import qin.com.coodinatorqin.searchFancy.SearchFancyActivity;
@@ -35,7 +36,7 @@ public class MainAdapter extends RecyclerView.Adapter<DataBindingViewHolder> {
     private Class[] activities = new Class[]{AppBarLayoutActivity.class,collapseLayoutActivity.class
     ,MeasureCollpaseActivity.class,CollapseSnapActivity.class,FloatButtonActivity.class
     ,ZhiHuActivity.class,BottomSheetBaiduActivity.class,SearchFancyActivity.class
-    , TabLayoutActivity.class};
+    , TabLayoutActivity.class,CollapseTestActivity.class};
     private final LayoutInflater mLayoutInflater;
     private ViewDataBinding mViewDataBinding;
 
